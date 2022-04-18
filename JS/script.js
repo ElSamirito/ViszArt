e8f7c38a9f40f874e788ae478bdc2ede32aa2eea
Samir Arab <arabsamir2003@gmail.com>
2022-04-18 20:38:24 -0300

    extended info 2, staff and basic form

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+function mostrar(dni){
+    incol = document.getElementById(dni);
+    
+    incol.style.display = "block";
+};
+
+function ocultar(dni){
+    incol = document.getElementById(dni);
+    
+    incol.style.display = "none";
+};
